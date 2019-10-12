b4c30df27ee8b3b1fd71a89c82657093046ee4e9
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2019-10-12 13:30:16 -0400

    Update SharedData.java
    Updated for sharing new block data

--- sharedData/SharedData.java
+++ sharedData/SharedData.java
@@ -4,5 +4,9 @@ public class SharedData {
 
 	public static int difficulty;
 	public static String lastBlock;
+	public static String previousHash;
+	public static String newHash;
+	
+	public static String newBlockString;
 	public static boolean newBlockFound = false;
 }
